174fbb3d5585dc38f5bc42a6e06ebc6227a2b248
Oleg An <oleg.an@sirenltd.com>
2024-04-01 01:27:27 +0700

    removed mocks

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,5 @@
 import { contractorsList } from './mock/contractors';
-import { states } from './mock/states';
 
 export function getContractors() {
   return Promise.resolve(contractorsList);
-}
-
-export function getStates() {
-  return Promise.resolve(states);
 }
