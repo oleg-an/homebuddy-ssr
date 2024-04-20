22f072a92ca0cf1b3eb5f368eb7349c9a6b8a111
Oleg An <oleg.an@sirenltd.com>
2024-04-20 12:20:10 +0700

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import {defineConfig} from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
     adapter: netlify(),
 });
---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function TestComponent() {
+  return (<div>
+      vds
+    </div>
+  );
+}
