48ddd26c5c848cc5a20b060ba32313cb8eaf66b6
Oleg An <oleg.an@sirenltd.com>
2024-04-20 16:16:47 +0700

    .

--- src/components/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function TestComponent() {
-  return (<div>
-      vds
-    </div>
-  );
-}
--- src/react-components/Wizard.tsx
+++ src/react-components/Wizard.tsx
@@ -6,4 +6,3 @@ export function Wizard({}) {
     <Button>Test</Button>
   </>;
 }
-
