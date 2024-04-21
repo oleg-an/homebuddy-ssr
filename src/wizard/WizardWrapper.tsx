0986b9241035e68a89f195b6efed472dbe5aaaae
Oleg An <oleg.an@sirenltd.com>
2024-04-21 11:52:22 +0700

    .

--- src/wizard/WizardWrapper.tsx
+++ src/wizard/WizardWrapper.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import style from './WizardWrapper.module.scss';
 
 interface WizardWrapperProps {
   children: React.ReactNode;
@@ -7,10 +8,6 @@ interface WizardWrapperProps {
 }
 
 export function WizardWrapper({ children }: WizardWrapperProps) {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-  }, []);
-
   return createPortal(<div className="top-0 left-0 w-screen h-screen bg-white fixed">
     <div className="bg-deep-blue-25 h-full">
       <div className="h-full container px-4 pt-4 position-relative">
@@ -18,8 +15,15 @@ export function WizardWrapper({ children }: WizardWrapperProps) {
           <img alt="" src="/header-logo.svg"/>
           <div>close</div>
         </div>
-        {children}
+        <div className={style.childrenWrapper}>
+          <div className={style.wrapper}>
+            {children}
+          </div>
+        </div>
       </div>
     </div>
-  </div>, document.body);
+  </div>, (() => {
+    document.body.style.overflow = 'hidden';
+    return document.body;
+  })());
 }
