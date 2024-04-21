f7672bc22d2ecc50c23579fadc154d9c93687120
Oleg An <oleg.an@sirenltd.com>
2024-04-21 10:33:10 +0700

    .

--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -2,7 +2,6 @@ import type { CreateLeadResponseModel } from '../model';
 import { getWizardProps, renderWizard } from 'api/utils.ts';
 
 export function Wizard({ leadResponse }: { leadResponse: CreateLeadResponseModel }) {
-  console.log(leadResponse);
   return <div>{leadResponse.uuid}</div>;
 }
 
